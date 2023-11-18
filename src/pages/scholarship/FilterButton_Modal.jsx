66015e8a5fe04e29672cc9c84472d4883692ff56
bryanhad <bryanhadinata@gmail.com>
2023-11-18 15:15:47 +0700

    feat: add modal to filter button on mobile mode in scholarships page

---
+++ src/pages/scholarship/FilterButton_Modal.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2"
+import { CgClose } from "react-icons/cg"
+import FilterMenu from "./FilterMenu"
+
+export default function FilterButton_Modal() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    console.log(isOpen)
+    return (
+        <>
+            <button
+                className="h-full md:hidden aspect-square border border-primary text-primary grid place-content-center rounded-xl"
+                onClick={() => setIsOpen(true)}
+            >
+                <HiOutlineAdjustmentsHorizontal className="text-4xl" />
+            </button>
+            {isOpen && (
+                <>
+                    <div
+                        onClick={() => setIsOpen(false)}
+                        className="fixed md:hidden z-[100] top-0 left-0 w-full h-screen bg-slate-500/30"
+                    ></div>
+                </>
+            )}
+            <article
+                className={`fixed md:hidden z-[101] bottom-0 left-0 w-full rounded-t-3xl bg-white duration-500 ${
+                    isOpen ? "max-h-[100vh] p-6" : "max-h-[0vh]"
+                }`}
+            >
+                <button
+                    className="absolute right-6 top-6"
+                    onClick={() => setIsOpen(false)}
+                >
+                    <CgClose className="text-3xl text-slate-500" />
+                </button>
+                <FilterMenu />
+            </article>
+        </>
+    )
+}
--- src/pages/scholarship/FilterModalButton.jsx
+++ src/pages/scholarship/FilterModalButton.jsx
@@ -1,9 +1,1 @@
-import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2"
-
-export default function FilterModalButton() {
-  return (
-    <button className="grid h-full aspect-square md:hidden place-content-center border border-primary rounded-xl text-primary">
-        <HiOutlineAdjustmentsHorizontal className="text-4xl" />
-    </button>
-  )
-}
+// import { useState } from "react"

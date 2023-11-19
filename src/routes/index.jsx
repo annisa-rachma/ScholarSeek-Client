26e438974ba4befee35b0a0b7af0ed32460ce3d6
bryanhad <bryanhadinata@gmail.com>
2023-11-19 11:38:13 +0700

    feat: add mentoring page add working search bar add infinite scrolling for mentoring cards

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom"
 import RootLayout from "../layouts/RootLayout"
 import Home from "../pages/home/Home"
-import About from "../pages/About"
-import Scholarships from "../pages/scholarship/main/Scholarships"
-import ScholarshipDetail from "../pages/scholarship/detail/ScholarshipDetail"
+import AboutPage from "../pages/AboutPage"
+import ScholarshipsPage from "../pages/scholarship/main/ScholarshipsPage"
+import ScholarshipDetailPage from "../pages/scholarship/detail/ScholarshipDetailPage"
+import MentoringPage from "../pages/mentoring/main/MentoringPage"
+import MentoringDetaiLPage from "../pages/mentoring/detail/MentoringDetaiLPage"
 
 export const router = createBrowserRouter([
     {
@@ -16,15 +18,23 @@ export const router = createBrowserRouter([
             },
             {
                 path: "/about",
-                element: <About />,
+                element: <AboutPage />,
             },
             {
                 path: "/scholarships",
-                element: <Scholarships />,
+                element: <ScholarshipsPage />,
             },
             {
                 path: '/scholarships/:slug',
-                element: <ScholarshipDetail/>
+                element: <ScholarshipDetailPage/>
+            },
+            {
+                path: '/mentoring',
+                element: <MentoringPage/>
+            },
+            {
+                path: '/mentoring/:slug',
+                element: <MentoringDetaiLPage/>
             }
         ],
     },

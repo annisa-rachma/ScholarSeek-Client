3ed148847410a8c7a899dea9aacb472cdadf0e96
annisarachma <saa.annisa15@gmail.com>
2023-11-20 15:40:21 +0700

    feat: add login page

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import "./index.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Provider } from 'react-redux'
 import store from "./stores/index.js"
-
+import { ToastContainer } from 'react-toastify';
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
     <QueryClientProvider client={queryClient}>
         <App />
+        <ToastContainer />
     </QueryClientProvider>
     </Provider>
 )
